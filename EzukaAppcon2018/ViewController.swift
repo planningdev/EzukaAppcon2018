c98e40c1bf601b873345b5d148d5be9d36bd545d
Atsushi KONISHI <crossover.ak@outlook.jp>
2018-11-03 23:22:38 +0900

    Initial Commit

---
+++ EzukaAppcon2018/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EzukaAppcon2018
+//
+//  Created by Atsushi KONISHI on 2018/11/03.
+//  Copyright © 2018 小西篤志. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
